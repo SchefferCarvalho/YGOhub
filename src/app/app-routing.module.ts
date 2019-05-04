835e5a7460e3af8d23dd46b9644ade577ae1ddf9
Dan <daniloplay2009@hotmail.com>
2019-05-04 12:00:21 -0300

    versão 0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CriarDecksComponent } from './criar-decks/criar-decks.component';
+import { PagCartasComponent } from './pag-cartas/pag-cartas.component';
+import { PagFavoritoComponent } from './pag-favorito/pag-favorito.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+const APP_ROUTES: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'criar-decks', component: CriarDecksComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'pag-cartas', component: PagCartasComponent},
+  {path: 'pag-decks', component: PagCartasComponent},
+  {path: 'pag-favorito', component: PagFavoritoComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarDecksComponent } from './criar-decks/criar-decks.component';
 import { LoginComponent } from './login/login.component';
-import { PagInicioComponent } from './pag-inicio/pag-inicio.component';
 import { PagCartasComponent } from './pag-cartas/pag-cartas.component';
 import { PagDecksComponent } from './pag-decks/pag-decks.component';
 import { PagFavoritoComponent } from './pag-favorito/pag-favorito.component';
-import { CriarDecksComponent } from './criar-decks/criar-decks.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { routing } from './app-routing.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CadastroComponent,
+    CriarDecksComponent,
     LoginComponent,
-    PagInicioComponent,
     PagCartasComponent,
     PagDecksComponent,
     PagFavoritoComponent,
-    CriarDecksComponent,
+    InicioComponent,
     PerfilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
